61a6f15417f96a647b4b7ff4b0ce81a6641caae8
daniloti1 <danilo.ti@hotmail.com>
2017-09-03 01:12:21 -0500

    Se establece el servidor

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-taller12: taller12.c
-	gcc -Wall servidor.c -o servidor -ludev
+servidor: servidor.c
+	gcc -Wall servidor.c -o servidor -ludev -lmicrohttpd
+	cc servidor.c -o servidor_segun_tutorial -I$PATH_TO_LIBMHD_INCLUDES -L$PATH_TO_LIBMHD_LIBS -lmicrohttpd
 clean:
 	rm servidor
 
--- servidor.c
+++ servidor.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <mntent.h>
 #include <libudev.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <microhttpd.h>
 
+#define PORT 8888
 
+/*
 struct udev_device* obtener_hijo(struct udev* udev,struct udev_device* padre,const char* subsistema)
 {
 	struct udev_device* hijo = NULL;
@@ -73,9 +80,36 @@ static char* enumerar_disp_alm_masivo(struct udev* udev)
 	udev_enumerate_unref(enumerar);
 	return devnode;
 }
+*/
+
+void request_completed (void *cls, struct MHD_Connection *connection, 
+     		        void **con_cls,
+                        enum MHD_RequestTerminationCode toe)
+{
+  	printf("funciono");  
+}
+
+int answer_to_connection (void *cls, struct MHD_Connection *connection,
+                          const char *url,
+                          const char *method, const char *version,
+                          const char *upload_data,
+                          size_t *upload_data_size, void **con_cls) 
+{
+  	const char *page  = "<html><body>Hello, browser!</body></html>";
+
+	struct MHD_Response *response;
+  	int ret;
+
+  	response = MHD_create_response_from_buffer (strlen (page), (void*) page, MHD_RESPMEM_PERSISTENT);
+
+	ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+ 	MHD_destroy_response (response);
+
+  	return ret;
+}
 
 int main(int argc, char** argv) {
-	struct udev *udev = udev_new();
+	//struct udev *udev = udev_new();
 
 	/*
 	struct udev_enumerate *enumerate;
@@ -86,9 +120,19 @@ int main(int argc, char** argv) {
 	int fd;
 	*/
 
-	enumerar_disp_alm_masivo(udev);
+	//enumerar_disp_alm_masivo(udev);
+
+	
 
 
+	struct MHD_Daemon* daemon;
 
+	daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL,&answer_to_connection,NULL,MHD_OPTION_NOTIFY_COMPLETED, request_completed,NULL, MHD_OPTION_END);
+
+	getchar ();
+
+  	MHD_stop_daemon (daemon);
+
+	
 
 }
